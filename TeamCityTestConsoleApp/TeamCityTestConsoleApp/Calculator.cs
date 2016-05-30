2fe475752322e6139f45dd48ec377a60fc939651
James O'Keeffe <jaokeeffe@outlook.com>
2016-05-30 22:00:21 +1000

    Added a comment

--- TeamCityTestConsoleApp/TeamCityTestConsoleApp/Calculator.cs
+++ TeamCityTestConsoleApp/TeamCityTestConsoleApp/Calculator.cs
@@ -13,10 +13,12 @@ namespace TeamCityTestConsoleApp
         }
         public int Add(int a, int b)
         {
+            // Add two numbers
             return a + b;
         }
         public int Subtract(int a, int b)
         {
+            // Subtract two numbers
             return a - b;
         }
     }
