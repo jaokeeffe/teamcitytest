a4e5d6b0fedbe1fed2ab2300d23802a169a8a445
James O'Keeffe <jaokeeffe@outlook.com>
2016-05-30 21:35:21 +1000

    Test build

---
+++ TeamCityTestConsoleApp/TeamCityTestConsoleApp/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamCityTestConsoleApp
+{
+    class Calculator
+    {
+        Calculator()
+        {
+
+        }
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+        int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
